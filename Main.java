622b3c47570d138da9c9dc957c5435df9ad07c57
Kaname Takahashi <kaname.takahashi.sys21@morijyobi.ac.jp>
2022-03-17 13:33:52 +0900

    コミットしたよ

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+	public static void main(String[] args) {
+		double height = 1.6;
+		double weight = 50.3;
+		double bmi = weight/(height*height);
+
+		System.out.print(bmi);
+	
+	}
+}
+
+
